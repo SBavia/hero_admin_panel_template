9eace1286a8d81dc6f49f3d41ea51334275a9564
Ivan Nikanau <34274275+SBavia@users.noreply.github.com>
2023-01-19 23:10:25 +0300

    Add files via upload

---
+++ src/components/heroesFilters/HeroesFilters.js
@@ -0,0 +1,65 @@
+// import {useHttp} from '../../hooks/http.hook';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import classNames from 'classnames';
+import store from '../../store';
+
+import { fetchFilters, filtersChanged, selectAll  } from './filtersSlice';
+import Spinner from '../spinner/Spinner';
+
+const HeroesFilters = () => {
+
+    const {filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
+    const dispatch = useDispatch();
+    // const {request} = useHttp();
+
+    // Запрос на сервер для получения фильтров и последовательной смены состояния
+    useEffect(() => {
+        dispatch(fetchFilters());
+        // eslint-disable-next-line
+    }, []);
+
+    if (filtersLoadingStatus === "loading") {
+        return <Spinner/>;
+    } else if (filtersLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+    const renderFilters = (arr) => {
+        if (arr.length === 0) {
+            return <h5 className="text-center mt-5">Фильтры не найдены</h5>
+        }
+
+        // Данные в json-файле я расширил классами и текстом
+        return arr.map(({name, className, label}) => {
+
+            // Используем библиотеку classnames и формируем классы динамически
+            const btnClass = classNames('btn', className, {
+                'active': name === activeFilter
+            });
+            
+            return <button 
+                        key={name} 
+                        id={name} 
+                        className={btnClass}
+                        onClick={() => dispatch(filtersChanged(name))}
+                        >{label}</button>
+        })
+    }
+
+    const elements = renderFilters(filters);
+
+    return (
+        <div className="card shadow-lg mt-4">
+            <div className="card-body">
+                <p className="card-text">Отфильтруйте героев по элементам</p>
+                <div className="btn-group">
+                    {elements}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroesFilters;
